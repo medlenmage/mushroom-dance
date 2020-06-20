8e4a24aa07d7f186a231e3c3c9432769d2c4a818
Joshua Medlen <joshua246@gmail.com>
2020-06-20 12:05:32 -0500

    made login and logout functional

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,24 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import 'bootstrap';
 import '../styles/main.scss';
+import auth from './components/auth/auth';
+import myNav from './components/mynavbar/myNavBar';
+import authData from './helpers/data/authData';
+
+/**
+ * Be able to login to our app
+ * be able to logout
+ * see a login button if logged out
+ * see a logout button if logged in
+ * see a list of mushrooms if we are logged in
+ */
 
 const init = () => {
-  console.error('here we go');
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLogStatus();
+  auth.loginBtn();
+  myNav.logoutEvent();
 };
 
 init();
