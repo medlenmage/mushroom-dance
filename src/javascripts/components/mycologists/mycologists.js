1da3cc26de26c9fb536aa5f2a4fddfc01da8b145
Joshua Medlen <joshua246@gmail.com>
2020-06-23 18:46:10 -0500

    corrected cards

--- src/javascripts/components/mycologists/mycologists.js
+++ src/javascripts/components/mycologists/mycologists.js
@@ -1,10 +1,12 @@
 const mycologistMaker = (mycologist) => {
   const domString = `
   <div class="col-3">
-  <div class="card myco-card border-0 rounded-0" id=${mycologist.id}>
-    <div class="card-body">
-      <h5 class="card-title text-center">${mycologist.name}</h5>
+    <div class="card myco-card border-0 rounded-0" id=${mycologist.id}>
+      <div class="card-body">
+        <h5 class="card-title text-center">${mycologist.name}</h5>
+      </div>
     </div>
+  </div>
   `;
 
   return domString;
